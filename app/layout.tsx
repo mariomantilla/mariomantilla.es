3f5237f7d939467ffb3e1853798dc395296a0238
Mario Mantilla <mariomantillasanchez@gmail.com>
2024-08-24 00:27:36 +0200

    Added speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 const open_sans = Open_Sans({ subsets: ["latin"] });
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={open_sans.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
