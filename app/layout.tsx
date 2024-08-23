0d27b2bba2f72c4c969cc7f22f423acbc551346b
Mario Mantilla <mariomantillasanchez@gmail.com>
2024-08-23 23:48:36 +0200

    Added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 const open_sans = Open_Sans({ subsets: ["latin"] });
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={open_sans.className}>{children}</body>
+      <body className={open_sans.className}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
