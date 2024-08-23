232c5d5f172a7736a632fda670517eae52ff7552
Mario Mantilla <mariomantillasanchez@gmail.com>
2024-08-23 22:20:24 +0200

    Added about me and main skills and contact form logic

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Github, Linkedin, Mail, Send } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { Github, Linkedin, Mail, Send, SquareArrowOutUpRight, Chrome } from "lucide-react"
+import { buttonVariants } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { AnimatedLogo } from "@/components/animated/logo"
 import { AnimatedLi } from "@/components/animated/li"
@@ -11,12 +11,124 @@ import { ContactButton } from "@/components/contact-button"
 import { Avatar, AvatarImage } from "@/components/ui/avatar"
 import Image from "next/image"
 import marioPic from "@/public/mario.jpg"
+import { ContactForm } from "@/components/contact-form"
+import { Toaster } from "@/components/ui/toaster"
 
 
 export default function Home() {
 
+	const skills = [
+		"Python",
+		"JS/TS",
+		"Django",
+		"Flask",
+		"PostgreSQL",
+		"Node",
+		"Docker",
+		"Next.js (React)",
+		"Flutter",
+		"Angular",
+		"GitHub/GitLab",
+		"AWS/GCP"
+	]
+
+	const projects = [
+		{
+			title: "Honestore",
+			description: "A community of consumer activists. Web built using NextJS and deployed with Vercel. Mobile app made with Flutter and backend powered by Supabase.",
+			links: [
+				{
+					text: "Website",
+					url: "https://honestore.app/",
+					icon: <SquareArrowOutUpRight className="mr-2 h-5 w-5" />
+				},
+				{
+					text: "App Repo",
+					url: "https://github.com/mariomantilla/honestore",
+					icon: <Github className="mr-2 h-5 w-5" />
+				},
+				{
+					text: "Web Repo",
+					url: "https://github.com/mariomantilla/honestore-web",
+					icon: <Github className="mr-2 h-5 w-5" />
+				}
+			],
+			repos: [""]
+		},
+		{
+			title: "Data Visualization Dashboard",
+			description: "Webapp to post (via both API and web UI), visualize metrics on a timeline and see average values per minute, hour, and day. Using Next.js 14 (App Router) full-stack.",
+			links: [
+				{
+					text: "Web Repo",
+					url: "https://github.com/mariomantilla/metrics-dashboard",
+					icon: <Github className="mr-2 h-5 w-5" />
+				}
+			]
+		},
+		{
+			title: "Shifts Management App",
+			description: "Small webapp for scheduling shifts at work. Made with React and Supabase.",
+			links: [
+				{
+					text: "Website",
+					url: "https://workschedules.netlify.app/",
+					icon: <SquareArrowOutUpRight className="mr-2 h-5 w-5" />
+				},
+				{
+					text: "Web Repo",
+					url: "https://github.com/mariomantilla/schedules",
+					icon: <Github className="mr-2 h-5 w-5" />
+				}
+			]
+		},
+		{
+			title: "Gitlab Time Tracker",
+			description: "Chrome Extension to track the time you spend in each GitLab issue (task)",
+			links: [
+				{
+					text: "Chrome Web Store",
+					url: "https://chromewebstore.google.com/detail/gitlab-time-tracker/dfgcimjdeijmapdncmgndgldefodhflo?hl=es&pli=1",
+					icon: <Chrome className="mr-2 h-5 w-5" />
+				}
+			]
+		},
+		{
+			title: "Hacker News Scraper",
+			description: "Web crawler developed in Python that extracts information from the Hacker News webpage using scraping techniques",
+			links: [
+				{
+					text: "Web Repo",
+					url: "https://github.com/mariomantilla/hacker-news-scraper",
+					icon: <Github className="mr-2 h-5 w-5" />
+				}
+			]
+		},
+		{
+			title: "Marketing Websites",
+			description: "Websites developed for SMEs and professionals to showcase their services and build online presence",
+			links: [
+				{
+					text: "Limpiaplus",
+					url: "https://limpiaplus.com/",
+					icon: <SquareArrowOutUpRight className="mr-2 h-5 w-5" />
+				},
+				{
+					text: "Webpros",
+					url: "https://www.webpros.es/",
+					icon: <SquareArrowOutUpRight className="mr-2 h-5 w-5" />
+				},
+				{
+					text: "Kamila",
+					url: "https://www.kamilaguilera.com/",
+					icon: <SquareArrowOutUpRight className="mr-2 h-5 w-5" />
+				}
+			]
+		},
+	]
+
 	return (
-		<div className="min-h-screen bg-gradient-to-br from-orange-50 via-orange-200 to-orange-100 text-stone-900">
+		<div className="min-h-screen bg-gradient-to-br via-rose-100 from-blue-100 to-orange-100 text-stone-900">
 			<header className="sticky top-0 z-10 backdrop-blur-sm bg-white/10 border-b border-white/20 max-w-screen-2xl">
 				<div className="container mx-auto px-4 py-4 flex justify-center sm:justify-between items-center">
 					<AnimatedLogo>
@@ -36,7 +148,7 @@ export default function Home() {
 				<section className="mb-24">
 					<h1 className="text-5xl sm:text-7xl mb-5">Mario Mantilla</h1>
 					<h2 className="text-2xl text-balance">A <span className="bg-cyan-950 text-white px-2">software engineer</span> with focus
-					on <span className="bg-rose-800 text-white px-2">social impact</span></h2>
+						on <span className="bg-rose-800 text-white px-2">social impact</span></h2>
 				</section>
 				<section id="about" className="mb-24 flex flex-row gap-10 max-sm:flex-col-reverse">
 					<AnimatedDiv
@@ -47,8 +159,13 @@ export default function Home() {
 					>
 						<h2 className="text-5xl mb-6">About Me</h2>
 						<p className="text-xl mb-6 text-balance">
-							Hi, I&apos;m Jane Doe, a passionate software engineer with 5 years of experience in full-stack development.
-							I specialize in building scalable web applications using modern technologies.
+							A versatile engineer that loves transforming software into social impact.
+							Background in aerospace engineering and years of experience developing software.
+							Worked as freelance developer and building scalable SaaS applications.
+							<br /><br />
+							Flexible character and team worker that can work independently but enjoys
+							teaching and learning from others. Savours diving into product design and business
+							logic to come up with the best technical solutions.
 						</p>
 						<div className="flex space-x-4">
 							<HeaderButton
@@ -68,7 +185,7 @@ export default function Home() {
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.8 }}
 					>
-						<Avatar style={{width: 250, height: 250}} className="mx-auto sm:mx-0">
+						<Avatar style={{ width: 250, height: 250 }} className="mx-auto sm:mx-0">
 							<AvatarImage asChild src="mario.jpg">
 								<Image priority src={marioPic} alt="Picture of Mario" width={250} height={250} />
 							</AvatarImage>
@@ -78,9 +195,9 @@ export default function Home() {
 				</section>
 
 				<section id="skills" className="mb-24">
-					<AnimatedH2>Skills</AnimatedH2>
+					<AnimatedH2>Main Technologies</AnimatedH2>
 					<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-						{["JavaScript", "React", "Node.js", "Python", "SQL", "Git", "AWS", "Docker"].map((skill, index) => (
+						{skills.map((skill, index) => (
 							<AnimatedDiv
 								key={skill}
 								initial={{ opacity: 0, scale: 0.8 }}
@@ -88,8 +205,8 @@ export default function Home() {
 								transition={{ duration: 0.5, delay: index * 0.1 }}
 							>
 								<Card className="bg-white/10 border-white/20 hover:bg-white/20 transition-colors duration-300">
-									<CardContent className="p-6">
-										<p className="text-center text-lg font-semibold text-stone-900">{skill}</p>
+									<CardContent className="p-2 sm:p-6">
+										<p className="break-words text-center text-sm sm:text-lg font-semibold text-stone-900">{skill}</p>
 									</CardContent>
 								</Card>
 							</AnimatedDiv>
@@ -99,29 +216,8 @@ export default function Home() {
 
 				<section id="projects" className="mb-24">
 					<AnimatedH2>Projects</AnimatedH2>
-					<div className="grid md:grid-cols-2 gap-8">
-						{[
-							{
-								title: "E-commerce Platform",
-								description: "A full-stack e-commerce solution built with React and Node.js.",
-								link: "#"
-							},
-							{
-								title: "Task Management App",
-								description: "A React Native mobile app for managing daily tasks and schedules.",
-								link: "#"
-							},
-							{
-								title: "Data Visualization Dashboard",
-								description: "An interactive dashboard using D3.js for visualizing complex datasets.",
-								link: "#"
-							},
-							{
-								title: "AI Chatbot",
-								description: "A machine learning powered chatbot built with Python and TensorFlow.",
-								link: "#"
-							}
-						].map((project, index) => (
+					<div className="grid md:grid-cols-2 gap-8 items-stretch">
+						{projects.map((project, index) => (
 							<AnimatedDiv
 								key={index}
 								initial={{ opacity: 0, y: 50 }}
@@ -133,8 +229,17 @@ export default function Home() {
 										<CardTitle className="text-2xl font-semibold text-stone-900">{project.title}</CardTitle>
 										<CardDescription className="text-stone-900/70">{project.description}</CardDescription>
 									</CardHeader>
-									<CardContent>
-										<Button variant="outline" className="">View Project</Button>
+									<CardContent className="flex gap-4 flex-wrap">
+										{project.links.map((link) => (
+											<a
+												key={link.text}
+												href={link.url}
+												target="_blank"
+												rel="noopener noreferrer"
+												className={buttonVariants({ variant: "outline" })}>
+												{link.icon}	{link.text}
+											</a>
+										))}
 									</CardContent>
 								</Card>
 							</AnimatedDiv>
@@ -142,37 +247,18 @@ export default function Home() {
 					</div>
 				</section>
 
-				<section id="contact" className="mb-24">
+				<section id="contact" className="mb-18">
 					<AnimatedH2>Contact Me</AnimatedH2>
 					<AnimatedDiv
 						initial={{ opacity: 0, y: 50 }}
 						animate={{ opacity: 1, y: 0 }}
 						transition={{ duration: 0.8 }}
 					>
-					<Card className="bg-white/10 border-white/20">
-						<CardContent className="p-8">
-							<form className="space-y-6 items-center">
-								<div>
-									<label htmlFor="name" className="block text-lg font-medium mb-2">Name</label>
-									<input type="text" id="name" className="w-full p-3 bg-white/5 border border-white/20 rounded-md placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-rose-900" />
-								</div>
-								<div>
-									<label htmlFor="email" className="block text-lg font-medium mb-2">Email</label>
-									<input type="email" id="email" className="w-full p-3 bg-white/5 border border-white/20 rounded-md placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-rose-900" />
-								</div>
-								<div>
-									<label htmlFor="message" className="block text-lg font-medium mb-2">Message</label>
-									<textarea id="message" rows={4} className="w-full p-3 bg-white/5 border border-white/20 rounded-md placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-rose-900"></textarea>
-								</div>
-								<div className="flex items-center justify-center">
-									<Button variant="outline" type="submit">
-										<Send className="mr-2 h-5 w-5" />
-										Send Message
-									</Button>
-								</div>
-							</form>
-						</CardContent>
-					</Card>
+						<Card className="bg-white/10 border-white/20">
+							<CardContent className="p-8">
+								<ContactForm />
+							</CardContent>
+						</Card>
 					</AnimatedDiv>
 				</section>
 			</main>
@@ -200,6 +286,7 @@ export default function Home() {
 				</div>
 			</footer>
 			<GoTop />
+			<Toaster />
 		</div>
 	)
 }
---
+++ components/contact-form.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import { Send } from "lucide-react"
+import { Button } from "./ui/button"
+import { useState } from "react";
+import { toast } from "./ui/use-toast";
+
+export const ContactForm = () => {
+
+    function sendEmail() {
+        fetch('/api/send-email', {
+            method: 'POST',
+            body: JSON.stringify({
+                name, email, message
+            }),
+        }).then((res) => res.json().then((response) => {
+            toast({
+                variant: res.status !== 500 ? 'default' : 'destructive',
+                description: response.message,
+            })
+            if (res.status !== 500) {
+                setName('')
+                setEmail('')
+                setMessage('')
+            }
+        }))
+        .catch((err) => {
+            toast({
+                variant: "destructive",
+                description: err,
+            })
+        });
+    }
+
+
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+
+    const inputClassName = "w-full p-3 bg-white/5 border border-white/20 rounded-md placeholder-white/50 focus:outline-none focus:ring-2 focus:ring-rose-900"
+
+    return (
+        <form className="space-y-6 items-center" action={sendEmail}>
+            <div>
+                <label htmlFor="name" className="block text-lg font-medium mb-2">Name</label>
+                <input type="text" id="name" name="name" className={inputClassName} value={name} onChange={
+                    (e) => setName(e.target.value)
+                } />
+            </div>
+            <div>
+                <label htmlFor="email" className="block text-lg font-medium mb-2">Email</label>
+                <input type="email" name="email" id="email" className={inputClassName} value={email} onChange={
+                    (e) => setEmail(e.target.value)
+                } />
+            </div>
+            <div>
+                <label htmlFor="message" className="block text-lg font-medium mb-2">Message</label>
+                <textarea name="message" id="message" rows={4} className={inputClassName} onChange={
+                    (e) => setMessage(e.target.value)
+                } value={message} />
+            </div>
+            <div className="flex items-center justify-center">
+                <Button variant="outline" type="submit">
+                    <Send className="mr-2 h-5 w-5" />
+                    Send Message
+                </Button>
+            </div>
+        </form>
+    )
+}
